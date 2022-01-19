8e1e8ab4173eb9211371282020eeb0081f90fdb7
elcaven <simon.masschelein@gmail.com>
2022-01-19 13:45:00 +0100

    Started creating catpuccin theme

---
+++ qtile/colorschemes/catpuccin.py
@@ -0,0 +1,52 @@
+rosewater = "#F5E0DC"
+flamingo = "#F2CDCD"
+mauve = "#DDB6F2"
+pink = "#F5C2E7"
+maroon = "#E8A2AF"
+red = "#F28FAD"
+peach = "#F8BD96"
+yellow = "#FAE3B0"
+green = "#ABE9B3"
+teal = "#B5E8E0"
+blue = "#96CDFB"
+sky = "#89DCEB"
+lavender = "#C9CBFF"
+black0 = "#161320"
+black1 = "#1A1826"
+black2 = "#1E1E2E"
+black3 = "#302D41"
+black4 = "#575268"
+gray0 = "#6E6C7E"
+gray1 = "#988BA2"
+gray2 = "#C3BAC6"
+white = "#D9E0EE"
+
+
+border_focus = mauve
+border_normal = black3
+
+foreground = '#ffffff'
+alternate_foreground = black4
+background = black0
+alternate_background = black2
+
+#highlight_color = ['44475a', '44475a']
+highlight_color = ['0f111a', '44475a']
+
+groupbox_urgent = maroon
+groupbox_current_screen_border = sky
+groupbox_screen_border = gray1
+groupbox_other_screen_border = gray1
+groupbox_other_current_screen_border = pink
+groupbox_inactive = black2
+
+widget_accent_foreground = sky
+widget_accent_background = gray1
+
+widget_current_layout = pink
+widget_window_count = pink
+
+block_highlight_text_color = gray0
+cpu_color = flamingo
+mem_color = green
+date_color = yellow
