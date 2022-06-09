38c03d170efab7c707d9a72570d9468bb0bbebe7
elcaven <simon.masschelein@gmail.com>
2022-06-09 14:42:56 +0200

    Updated qtile config

--- qtile/config.py
+++ qtile/config.py
@@ -3,16 +3,17 @@
 import os
 import subprocess
 import psutil
+import nerdfonts as nf
 
 from typing import List  # noqa: F401
 
-from libqtile import bar, hook
+from libqtile import bar, hook, widget
 from libqtile.config import Click, Drag, Group, Key, KeyChord, Match, Screen, ScratchPad, DropDown
 from libqtile.lazy import lazy
 from libqtile.log_utils import logger
 
 # Layout imports
-from libqtile.layout.stack import Stack
+from libqtile.layout.max import Max
 from libqtile.layout.floating import Floating
 from libqtile.layout.xmonad import MonadTall, MonadWide
 
@@ -28,7 +29,12 @@ from qtile_extras.widget import WindowName
 from qtile_extras.widget import Clock
 from qtile_extras.widget import Systray
 from qtile_extras.widget import Chord
+from qtile_extras.widget import GenPollText
 from qtile_extras.widget.decorations import RectDecoration
+
+from qtile_extras.popup.toolkit import (
+    PopupRelativeLayout, PopupImage, PopupText, PopupWidget
+)
 
 from colorschemes import catppuccin as colors
 #: }}}
@@ -38,7 +44,7 @@ from colorschemes import catppuccin as colors
 def autostart():
     logger.info("Startup hook called")
     home = os.path.expanduser('/home/simon/.config/qtile/autostart.sh')
-    subprocess.call([home])
+    subprocess.Popen([home])
 
 @hook.subscribe.screen_change
 def screen_change(event):
@@ -75,6 +81,52 @@ def float_to_front(qtile):
         for window in group.windows:
             if window.floating:
                 window.cmd_bring_to_front()
+
+def get_notification_status():
+    status = subprocess.check_output(['dunstctl', 'is-paused']).decode('utf-8').strip()
+    if status == "true":
+        return nf.icons["fa_bell_slash"]
+    else:
+        return nf.icons["fa_bell"]
+
+def toggle_notification_status():
+    subprocess.call(['dunstctl', 'set-paused', 'toggle'])
+
+def show_graphs(qtile):
+    controls = [
+        PopupWidget(
+            widget=widget.CPUGraph(),
+            width=0.45,
+            height=0.45,
+            pos_x=0.05,
+            pos_y=0.05
+        ),
+        PopupWidget(
+            widget=widget.NetGraph(),
+            width=0.45,
+            height=0.45,
+            pos_x=0.5,
+            pos_y=0.05
+        ),
+        PopupWidget(
+            widget=widget.MemoryGraph(),
+            width=0.9,
+            height=0.45,
+            pos_x=0.05,
+            pos_y=0.5
+        )
+    ]
+
+    layout = PopupRelativeLayout(
+        qtile,
+        width=1000,
+        height=200,
+        controls=controls,
+        background="00000060",
+        initial_focus=None,
+        close_on_click=False
+    )
+    layout.show(centered=True)
 #: }}}
 
 #: Key bindings {{{
@@ -86,25 +138,24 @@ scr_locker = "betterlockscreen -l"
 # Chord names
 window_resize = "window resize"
 window_move = "window move"
+spawn="spawn"
 
 keys = [
     Key([mod], "h", lazy.layout.left(), desc="Move focus to left"),
     Key([mod], "l", lazy.layout.right(), desc="Move focus to right"),
-    Key([mod], "j", lazy.group.next_window(), desc="Move focus down"),
-    Key([mod], "k", lazy.group.prev_window(), desc="Move focus up"),
-    Key([alt_key], "j", lazy.group.next_window(), desc="Move window focus to other window"),
+    Key([mod], "j", lazy.group.next_window(), desc="Move focus to next window"),
+    Key([mod], "k", lazy.group.prev_window(), desc="Move focus to previous window"),
     Key([mod], "u", lazy.next_urgent()),
     Key([mod, "shift"], "f", float_to_front),
 
-    Key([mod, "shift"], "h", lazy.layout.shuffle_left(), desc="Move window to the left"),
-    Key([mod, "shift"], "l", lazy.layout.shuffle_right(), desc="Move window to the right"),
-    Key([mod, "shift"], "j", lazy.layout.shuffle_down(), desc="Move window down"),
-    Key([mod, "shift"], "k", lazy.layout.shuffle_up(), desc="Move window up"),
+    #Key([mod, "shift"], "h", lazy.layout.shuffle_left(), desc="Move window to the left"),
+    #Key([mod, "shift"], "l", lazy.layout.shuffle_right(), desc="Move window to the right"),
+    #Key([mod, "shift"], "j", lazy.layout.shuffle_down(), desc="Move window down"),
+    #Key([mod, "shift"], "k", lazy.layout.shuffle_up(), desc="Move window up"),
 
-    Key([mod, "control"], "h", lazy.layout.grow(), desc="Grow window to the left"),
-    Key([mod, "control"], "l", lazy.layout.shrink(), desc="Grow window to the right"),
-    Key([mod], "n", lazy.layout.normalize(), desc="Reset all window sizes"),
-    Key([mod], "o", lazy.layout.maximize(), desc="Reset all window sizes"),
+    #Key([mod, "control"], "h", lazy.layout.grow(), desc="Grow window to the left"),
+    #Key([mod, "control"], "l", lazy.layout.shrink(), desc="Grow window to the right"),
+    #Key([mod], "o", lazy.layout.maximize(), desc="Reset all window sizes"),
     Key([mod, "control"], "Return", lazy.layout.flip()),
 
     Key([mod, "shift"], "Return", lazy.layout.toggle_split(), desc="Toggle between split and unsplit sides of stack"),
@@ -126,15 +177,12 @@ keys = [
     Key([], "XF86MonBrightnessUp", lazy.spawn("sudo ybacklight -inc 1")),
 
     Key([], "Print", lazy.spawn("flameshot gui")),
-    Key([mod], "d", lazy.spawn("emacs")),
-    Key([mod], "n", lazy.spawn("kitty -e lvim")),
-    Key([mod], "a", lazy.spawn("kitty -e lvim /home/simon/projects/personal/dotfiles/qtile/config.py")),
-    Key([mod], "e", lazy.spawn("kitty -e ranger")),
     Key([mod], "z", lazy.group['scratchpad'].dropdown_toggle('term')),
     Key([mod], "p", lazy.group['scratchpad'].dropdown_toggle('insomnia')),
     Key([mod], "o", lazy.group['scratchpad'].dropdown_toggle('browser')),
     Key([alt_key], "l", lazy.spawn(scr_locker)),
     Key([mod], "b", lazy.hide_show_bar("top")),
+    Key([mod, "shift"], "q", lazy.function(show_graphs)),
 
     # Dunst
     Key(["control"], "space", lazy.spawn("dunstctl close")),
@@ -145,15 +193,22 @@ keys = [
         Key([], "l", lazy.layout.grow()),
         Key([], "h", lazy.layout.shrink()),
         Key([], "n", lazy.layout.normalize()),
-        Key([], "m", lazy.layout.maximize())],
+        Key([], "m", lazy.layout.maximize(), desc="Reset all window sizes")],
         mode=window_resize
     ),
-    KeyChord([mod], "s", [
-        Key([], "h", lazy.layout.shuffle_left()),
-        Key([], "l", lazy.layout.shuffle_right()),
-        Key([], "j", lazy.layout.shuffle_down()),
-        Key([], "k", lazy.layout.shuffle_up())],
+    KeyChord([mod], "m", [
+        Key([], "h", lazy.layout.shuffle_left(), desc="Move window to the left"),
+        Key([], "l", lazy.layout.shuffle_right(), desc="Move window to the right"),
+        Key([], "j", lazy.layout.shuffle_down(), desc="Move window down"),
+        Key([], "k", lazy.layout.shuffle_up(), desc="Move window up")],
         mode=window_move
+    ),
+    KeyChord([mod], "s", [
+        Key([], "e", lazy.spawn("emacs")),
+        Key([], "l", lazy.spawn("kitty -e lvim")),
+        Key([], "r", lazy.spawn("kitty -e ranger")),
+        Key([], "q", lazy.spawn("kitty -e lvim /home/simon/projects/personal/dotfiles/qtile/config.py"))],
+        mode=spawn
     ),
 ]
 #: }}}
@@ -161,7 +216,7 @@ keys = [
 #: Groups {{{
 groups = [
     Group(name='1', layout='monadtall'),
-    Group(name='2', layout='stack', matches=[
+    Group(name='2', layout='max', matches=[
         Match(wm_class='jetbrains-idea'),
         Match(wm_class='jetbrains-toolbox')]),
     Group(name='3', layout='monadtall'),
@@ -169,14 +224,15 @@ groups = [
     Group(name='5', layout='monadtall'),
     Group(name='6', layout='monadtall'),
     Group(name='7', layout='monadtall'),
-    Group(name='8', layout='stack', matches=[
+    Group(name='8', layout='max', matches=[
         Match(wm_class='slack'),
         Match(wm_class='teams-for-linux'),
         Match(wm_class='microsoft teams - preview'),
         Match(wm_class='microsoft teams - insiders'),
         Match(wm_class='hexchat'),
         Match(wm_class='discord')]),
-    Group(name='9', layout='stack', matches=[Match(wm_class="evolution")]),
+    Group(name='9', layout='max', matches=[
+        Match(wm_class="evolution")]),
 ]
 
 for i in groups:
@@ -208,16 +264,25 @@ layout_theme = {
 }
 
 layouts = [
-    #layout.Max(**layout_theme),
-    Stack(margin=7, num_stacks=1, border_width=0),
+    Max(margin=7, border_width=0),
     MonadTall(**layout_theme, single_border_width=0, single_margin=7, ratio=0.6),
     MonadWide(**layout_theme, single_border_width=0, single_margin=7, ratio=0.6),
 ]
+
+floating_layout = Floating(
+    **layout_border,
+    float_rules=[
+        *Floating.default_float_rules,
+        Match(title='Reminders'),  # Evolution reminders
+        Match(wm_class='copyq')
+    ])
 #: }}}
 
 #: Bar {{{
 widget_defaults = dict(
-    font="JetBrainsMono Nerd Font Mono ExtraBold", fontsize=11, background=colors.background
+    font="JetBrainsMonoExtraBold Nerd Font Mono",
+    fontsize=11, 
+    background=colors.background
 )
 extension_defaults = widget_defaults.copy()
 
@@ -239,47 +304,26 @@ groupBoxWidget = GroupBox(
     other_current_screen_border=colors.groupbox_current_screen_border,
     highlight_color=colors.highlight_color,
     inactive=colors.groupbox_inactive,
-    borderwidth=2, margin_x=2, margin_y=4, padding_x=1,
-    spacing=4,
-    disable_drag=True,
-    hide_unused=False,
+    borderwidth=2, margin_x=2, margin_y=4, padding_x=1, spacing=4,
+    disable_drag=True, hide_unused=False,
     font="JuliaMono SemiBold"
 )
 
 chordWidget = Chord(
-    font="JetBrainsMono Nerd Font Mono ExtraBold", fontsize=11,
+    font="JetBrainsMonoExtraBold Nerd Font Mono",
+    fontsize=11,
+    width=7,
     background=colors.background,
     margin=5, padding=4, fmt="",
     chords_colors={
         '': (colors.background, 'ffffff'),
         window_resize: (colors.window_resize_chord_color, 'ffffff'),
         window_move: (colors.window_move_chord_color, 'ffffff'),
+        spawn: (colors.spawn_chord_color, 'ffffff'),
     }
 )
 
-decorationBar = bar.Bar(
-    [
-        Spacer(length=5),
-        groupBoxWidget,
-        Spacer(length=5),
-        CurrentLayout(**decor, fmt="{}", padding=10, foreground=colors.alternate_foreground, background=colors.widget_current_layout),
-        Spacer(length=5),
-        WindowCount(fmt="[{}]", foreground=colors.widget_current_layout, padding=0),
-        Spacer(length=3),
-        WindowName(for_current_screen=True, padding=0),
-        Spacer(length=8),
-        CPU(**decor, format="cpu {load_percent}%", padding=10, foreground=colors.alternate_foreground, background=colors.cpu_color),
-        Spacer(length=5),
-        Memory(**decor, format="mem {MemUsed:.0f}Mb", padding=10, foreground=colors.alternate_foreground, background=colors.mem_color),
-        Spacer(length=5),
-        Clock(**decor, format="%a %d %b, %H:%M", padding=10, foreground=colors.alternate_foreground, background=colors.date_color),
-        Spacer(length=5),
-        Systray(padding=2, background=colors.background),
-        Spacer(length=10),
-    ],
-    25
-)
-
+    
 blocksBar = bar.Bar(
     [
         chordWidget,
@@ -308,7 +352,8 @@ blocksBar = bar.Bar(
 
 simpleBar = bar.Bar(
     [
-        Spacer(length=5),
+        chordWidget,
+        Spacer(length=1),
         groupBoxWidget,
         Spacer(length=5),
         WindowCount(fmt="[{}]", padding=0, foreground=colors.widget_current_layout),
@@ -326,13 +371,17 @@ simpleBar = bar.Bar(
         Clock(format="%a %d %b, %H:%M", padding=0, margin_y=0),
         Spacer(length=12),
         Systray(padding=2, background=colors.background),
+        GenPollText(
+            func=get_notification_status, update_interval=0.1, 
+            mouse_callbacks={"Button1": toggle_notification_status},
+            fontsize=21, padding=4),
         Spacer(length=10),
     ],
     23,
 )
 
 screens = [
-    Screen(top=blocksBar)
+    Screen(top=simpleBar)
 ]
 #: }}}
 
@@ -348,19 +397,6 @@ dgroups_key_binder = None
 dgroups_app_rules = []  # type: List
 bring_front_click = False
 cursor_warp = False
-floating_layout = Floating(
-    **layout_border,
-    float_rules=[
-        *Floating.default_float_rules,
-        Match(wm_class='confirmreset'),  # gitk
-        Match(wm_class='makebranch'),  # gitk
-        Match(wm_class='maketag'),  # gitk
-        Match(wm_class='ssh-askpass'),  # ssh-askpass
-        Match(title='branchdialog'),  # gitk
-        Match(title='pinentry'),  # GPG key password entry
-        Match(title='Reminders'),  # Evolution reminders
-        Match(wm_class='copyq')
-    ])
 auto_fullscreen = True
 focus_on_window_activation = "focus"
 reconfigure_screens = True
